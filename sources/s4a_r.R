4ca3bd2f2e5711b6c47bfdd93a9b558cf07fd32d
Roger Bivand <roger.bivand@nhh.no>
2019-09-04 00:21:00 +0200

    progress s4a_r

--- sources/s4a_r.R
+++ sources/s4a_r.R
@@ -1,46 +1,24 @@
----
-title: 'R: Spatial regression'
-author: "Roger Bivand"
-date: "Thursday, 5 September 2019, 15:12-15:36"
-output:
-  html_document:
-    keep_md: yes
-  pdf_document: default
-link-citations: yes
-bibliography: rmd.bib
----
 
-### Required current contributed CRAN packages:
-
-I am running R 3.6.1, with recent `update.packages()`.
-
-```{r, echo=TRUE}
-needed <- c("sf", "stars", "sp", "classInt", "raster", "colorspace", "RColorBrewer", "ggplot2", "cartography", "tmap", "mapview")
-```
-
-### Beijing data set
-
-```{r}
+## ------------------------------------------------------------------------
 library(HSAR)
 library(sp)
 data(landSPDF)
 data(landprice)
 data(Beijingdistricts)
-```
 
 
-```{r}
+## ------------------------------------------------------------------------
 library(sf)
 land_sf <- st_as_sf(landSPDF)
 landprice_sf <- merge(land_sf, landprice, by="obs")
 (landprice_sf <- landprice_sf[order(landprice_sf$district.id.x),])
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 all.equal(landprice_sf$district.id.x, landprice_sf$district.id.y)
-```
 
-```{r, warning=FALSE}
+
+## ---- warning=FALSE------------------------------------------------------
 library(spatialreg)
 dnb1.5 <- spdep::dnearneigh(landprice_sf, 0, 1500, row.names=as.character(landprice_sf$obs))
 dists <- spdep::nbdists(dnb1.5, st_geometry(landprice_sf))
@@ -49,10 +27,9 @@ ozpo <- spdep::set.ZeroPolicyOption(TRUE)
 set.ZeroPolicyOption(TRUE)
 lw <- spdep::nb2listw(dnb1.5, glist=edists, style="W")
 hist(spdep::card(dnb1.5))
-```
 
 
-```{r}
+## ------------------------------------------------------------------------
 landprice_sf$fyear <- factor(landprice_sf$year + 2003)
 landprice_sf$price <- exp(landprice_sf$lnprice)
 landprice_sf$area <- exp(landprice_sf$lnarea)
@@ -62,101 +39,94 @@ landprice_sf$Dpark <- exp(landprice_sf$dpark)
 landprice_sf$Dele <- exp(landprice_sf$dele)
 landprice_sf$f_district.id <- factor(landprice_sf$district.id.x)
 (t1 <- table(table(landprice_sf$f_district.id)))
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 Beijingdistricts$id1 <- Beijingdistricts$id+1
 all.equal(unique(landprice_sf$district.id.x), Beijingdistricts$id1)
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 (Beijingdistricts_sf <- st_as_sf(Beijingdistricts))
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 t2 <- table(sapply(st_contains(Beijingdistricts_sf, landprice_sf), length))
 all.equal(t1, t2)
-```
 
 
-
-```{r, warning=FALSE, message=FALSE}
+## ---- warning=FALSE, message=FALSE---------------------------------------
 nb_M <- spdep::poly2nb(Beijingdistricts, queen=FALSE, row.names=as.character(Beijingdistricts$id1))
 M <- as(spdep::nb2listw(nb_M, style="B"), "CsparseMatrix")
 dim(M)
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 hist(spdep::card(nb_M))
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 form <- log(price) ~ log(area) + log(Dcbd) + log(Dele) + log(Dpark) + log(Dsubway) + 
   crimerate + popden + fyear
 OLS <- lm(form, data=landprice_sf)
 summary(OLS)
-```
 
 
-```{r}
+## ------------------------------------------------------------------------
 spdep::lm.morantest(OLS, listw=lw)
-```
 
 
-```{r}
+## ------------------------------------------------------------------------
 spdep::lm.LMtests(OLS, listw=lw, test="all")
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 SLX <- lmSLX(form, data=landprice_sf, listw=lw)
 summary(impacts(SLX))
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 spdep::lm.morantest(SLX, listw=lw)
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 spdep::lm.LMtests(SLX, listw=lw, test="all")
-```
-```{r}
+
+## ------------------------------------------------------------------------
 e <- eigenw(lw)
 SDEM <- errorsarlm(form, data=landprice_sf, listw=lw, Durbin=TRUE, control=list(pre_eig=e))
 summary(impacts(SDEM))
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 LR1.sarlm(SDEM)
-```
 
 
-```{r}
+## ------------------------------------------------------------------------
 Hausman.test(SDEM)
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 library(lme4)
 mlm_1 <- lmer(update(form, . ~ . + (1 | f_district.id)), data=landprice_sf, REML=FALSE)
 Beijingdistricts_sf$mlm_re <- ranef(mlm_1)[[1]][,1]
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 library(Matrix)
 suppressMessages(library(MatrixModels))
 Delta <- as(model.Matrix(~ -1 + f_district.id, data=landprice_sf, sparse=TRUE), "dgCMatrix")
-```
 
-```{r, warning=FALSE}
+
+## ---- warning=FALSE------------------------------------------------------
 m_hsar <- hsar(form, data=landprice_sf, W=NULL, M=M, Delta=Delta, burnin=500, Nsim=5000, thinning=1)
 Beijingdistricts_sf$hsar_re <- m_hsar$Mus[1,]
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 plot(Beijingdistricts_sf[,"mlm_re"])
-```
 
 
-```{r}
+## ------------------------------------------------------------------------
 plot(Beijingdistricts_sf[,"hsar_re"])
-```
 
