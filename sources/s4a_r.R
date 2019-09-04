9ed1eeca4321601eabd61279056ced31c470a5a4
Roger Bivand <roger.bivand@nhh.no>
2019-09-04 10:39:53 +0200

    progress s4a_r

--- sources/s4a_r.R
+++ sources/s4a_r.R
@@ -1,3 +1,6 @@
+## ---- echo=TRUE----------------------------------------------------------
+needed <- c("MatrixModels", "lme4", "spatialreg", "spdep", "sf", "sp", "HSAR")
+
 
 ## ------------------------------------------------------------------------
 library(HSAR)
@@ -21,10 +24,11 @@ all.equal(landprice_sf$district.id.x, landprice_sf$district.id.y)
 ## ---- warning=FALSE------------------------------------------------------
 library(spatialreg)
 dnb1.5 <- spdep::dnearneigh(landprice_sf, 0, 1500, row.names=as.character(landprice_sf$obs))
+dnb1.5
 dists <- spdep::nbdists(dnb1.5, st_geometry(landprice_sf))
 edists <- lapply(dists, function(x) exp((-((x/1000)^2))/(1.5^2)))
 ozpo <- spdep::set.ZeroPolicyOption(TRUE)
-set.ZeroPolicyOption(TRUE)
+oo <- set.ZeroPolicyOption(TRUE)
 lw <- spdep::nb2listw(dnb1.5, glist=edists, style="W")
 hist(spdep::card(dnb1.5))
 
@@ -42,6 +46,10 @@ landprice_sf$f_district.id <- factor(landprice_sf$district.id.x)
 
 
 ## ------------------------------------------------------------------------
+sapply(as.data.frame(landprice_sf[, c("price", "area", "Dcbd", "Dele", "Dpark", "Dsubway", "crimerate", "popden")]), function(x) length(unique(x)))
+
+
+## ------------------------------------------------------------------------
 Beijingdistricts$id1 <- Beijingdistricts$id+1
 all.equal(unique(landprice_sf$district.id.x), Beijingdistricts$id1)
 
@@ -51,18 +59,12 @@ all.equal(unique(landprice_sf$district.id.x), Beijingdistricts$id1)
 
 
 ## ------------------------------------------------------------------------
-t2 <- table(sapply(st_contains(Beijingdistricts_sf, landprice_sf), length))
-all.equal(t1, t2)
-
-
-## ---- warning=FALSE, message=FALSE---------------------------------------
-nb_M <- spdep::poly2nb(Beijingdistricts, queen=FALSE, row.names=as.character(Beijingdistricts$id1))
-M <- as(spdep::nb2listw(nb_M, style="B"), "CsparseMatrix")
-dim(M)
+Beijingdistricts_sf$counts <- sapply(st_contains(Beijingdistricts_sf, landprice_sf), length)
 
 
 ## ------------------------------------------------------------------------
-hist(spdep::card(nb_M))
+t2 <- table(Beijingdistricts_sf$counts)
+all.equal(t1, t2)
 
 
 ## ------------------------------------------------------------------------
@@ -77,11 +79,11 @@ spdep::lm.morantest(OLS, listw=lw)
 
 
 ## ------------------------------------------------------------------------
-spdep::lm.LMtests(OLS, listw=lw, test="all")
+spdep::lm.LMtests(OLS, listw=lw, test=c("RLMerr", "RLMlag"))
 
 
 ## ------------------------------------------------------------------------
-SLX <- lmSLX(form, data=landprice_sf, listw=lw)
+SLX <- lmSLX(form, data=landprice_sf, listw=lw, Durbin= ~ log(area) + log(Dcbd) + log(Dele) + log(Dpark) + log(Dsubway) + crimerate + popden)
 summary(impacts(SLX))
 
 
@@ -90,11 +92,12 @@ spdep::lm.morantest(SLX, listw=lw)
 
 
 ## ------------------------------------------------------------------------
-spdep::lm.LMtests(SLX, listw=lw, test="all")
+spdep::lm.LMtests(SLX, listw=lw, test=c("RLMerr", "RLMlag"))
+
 
 ## ------------------------------------------------------------------------
 e <- eigenw(lw)
-SDEM <- errorsarlm(form, data=landprice_sf, listw=lw, Durbin=TRUE, control=list(pre_eig=e))
+SDEM <- errorsarlm(form, data=landprice_sf, listw=lw, Durbin= ~ log(area) + log(Dcbd) + log(Dele) + log(Dpark) + log(Dsubway) + crimerate + popden, control=list(pre_eig=e))
 summary(impacts(SDEM))
 
 
@@ -118,6 +121,27 @@ suppressMessages(library(MatrixModels))
 Delta <- as(model.Matrix(~ -1 + f_district.id, data=landprice_sf, sparse=TRUE), "dgCMatrix")
 
 
+## ------------------------------------------------------------------------
+library(mapview)
+mapview(Beijingdistricts_sf)
+
+
+## ------------------------------------------------------------------------
+opar <- par(bg="lightgreen")
+plot(Beijingdistricts_sf[, "counts"])
+par(opar)
+
+
+## ---- warning=FALSE, message=FALSE---------------------------------------
+nb_M <- spdep::poly2nb(Beijingdistricts, queen=FALSE, row.names=as.character(Beijingdistricts$id1))
+M <- as(spdep::nb2listw(nb_M, style="B"), "CsparseMatrix")
+dim(M)
+
+
+## ------------------------------------------------------------------------
+hist(spdep::card(nb_M))
+
+
 ## ---- warning=FALSE------------------------------------------------------
 m_hsar <- hsar(form, data=landprice_sf, W=NULL, M=M, Delta=Delta, burnin=500, Nsim=5000, thinning=1)
 Beijingdistricts_sf$hsar_re <- m_hsar$Mus[1,]
@@ -129,4 +153,39 @@ plot(Beijingdistricts_sf[,"mlm_re"])
 
 ## ------------------------------------------------------------------------
 plot(Beijingdistricts_sf[,"hsar_re"])
+
+
+## ------------------------------------------------------------------------
+o <- match(landprice_sf$district.id.x, Beijingdistricts_sf$id1)
+landprice_sf$id1 <- Beijingdistricts_sf$id1[o]
+all.equal(landprice_sf$district.id.x, landprice_sf$id1)
+
+
+## ------------------------------------------------------------------------
+landprice_sf$mlm_re <- Beijingdistricts_sf$mlm_re[o]
+landprice_sf$hsar_re <- Beijingdistricts_sf$hsar_re[o]
+
+
+## ------------------------------------------------------------------------
+spdep::lm.morantest(lmSLX(update(form, . ~ . + mlm_re), data=landprice_sf, listw=lw, Durbin= ~ log(area) + log(Dcbd) + log(Dele) + log(Dpark) + log(Dsubway) + crimerate + popden), listw=lw)
+
+
+## ------------------------------------------------------------------------
+spdep::lm.morantest(lmSLX(update(form, . ~ . + hsar_re), data=landprice_sf, listw=lw, Durbin= ~ log(area) + log(Dcbd) + log(Dele) + log(Dpark) + log(Dsubway) + crimerate + popden), listw=lw)
+
+
+## ------------------------------------------------------------------------
+SDEM1 <- errorsarlm(update(form, . ~ . + mlm_re), data=landprice_sf, listw=lw, Durbin= ~ log(area) + log(Dcbd) + log(Dele) + log(Dpark) + log(Dsubway) + crimerate + popden, control=list(pre_eig=e))
+
+
+## ------------------------------------------------------------------------
+LR1.sarlm(SDEM1)
+
+
+## ------------------------------------------------------------------------
+SDEM2 <- errorsarlm(update(form, . ~ . + hsar_re), data=landprice_sf, listw=lw, Durbin= ~ log(area) + log(Dcbd) + log(Dele) + log(Dpark) + log(Dsubway) + crimerate + popden, control=list(pre_eig=e))
+
+
+## ------------------------------------------------------------------------
+LR1.sarlm(SDEM2)
 
