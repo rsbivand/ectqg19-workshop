b130ca98aaa4bc1f829708ac1bc343da8443ceba
Roger Bivand <roger.bivand@nhh.no>
2019-09-04 12:43:24 +0200

    progress s3b_r

--- sources/s3b_r.R
+++ sources/s3b_r.R
@@ -1,72 +1,49 @@
----
-title: 'R: Spatial autocorrelation'
-author: "Roger Bivand"
-date: "Thursday, 5 September 2019, 14:15-14:30"
-output:
-  pdf_document: default
-  html_document:
-link-citations: yes
-bibliography: rmd.bib
----
-
-
-
-```{r}
+## ------------------------------------------------------------------------
 library(sf)
 lux <- st_read("../data/lux_tmerc.gpkg")
-```
 
 
-```{r}
+## ------------------------------------------------------------------------
 library(spdep)
 nb_cont <- poly2nb(lux, row.names=as.character(lux$LAU2))
 lw_B <- nb2listw(nb_cont, style="B")
 lw_W <- nb2listw(nb_cont) # default style="W"
-```
 
 
-```{r}
+## ------------------------------------------------------------------------
 moran.test(lux$light_level, listw=lw_B, randomisation=TRUE, alternative="two.sided")
-```
 
 
-```{r}
+## ------------------------------------------------------------------------
 moran.test(lux$light_level, listw=lw_W, randomisation=TRUE, alternative="two.sided")
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 set.seed(1)
 perm_boot <- moran.mc(lux$light_level, listw=lw_W, nsim=9999, return_boot=TRUE)
 c(mean=mean(perm_boot$t), var=var(perm_boot$t))
 plot(perm_boot)
-```
 
 
-
-```{r}
+## ------------------------------------------------------------------------
 moran.plot(lux$light_level, listw=lw_W)
-```
 
 
-```{r}
+## ------------------------------------------------------------------------
 OLS <- lm(light_level ~  pop_den, lux)
 summary(OLS)
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 lm.morantest(OLS, listw=lw_W, alternative="two.sided")
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 moran.plot(residuals(OLS), listw=lw_W)
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 locm <- localmoran(lux$light_level, listw=lw_W, alternative="two.sided")
 lux$locIz <- locm[,4]
 plot(lux[,"locIz"])
-```
-
-
-
 
